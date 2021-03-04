b8c2d35866faf58e5791d8b992f13797a35894bc
Carter Warren <cwarren2023@chadwickschool.org>
2021-03-04 10:28:27 -0800

    Updated script for logs

--- scripts/start-entry2.sh
+++ scripts/start-entry2.sh
@@ -5,8 +5,8 @@ FILENAME="$(echo "logs/$(date +'%Y-%m-%d').log.md")"
 touch $FILENAME
 echo " " >> $FILENAME
 echo "# $(date +'%A, %B %e, %Y %I:%M %p')" >> $FILENAME
-echo "- [ ] " >> $FILENAME
-echo "- [ ] " >> $FILENAME
+echo "- [X] " >> $FILENAME
+echo "- [X] " >> $FILENAME
 echo " " >> $FILENAME
 echo "This is a sign-off log. I finished everything I wanted to." >> $FILENAME
 
